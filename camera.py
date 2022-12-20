ff482904636298bfce6208d05f44d34a1d9bec79
barabum0 <r.m.poltorabatko@gmail.com>
2022-12-20 19:11:35 +0300

    init

--- camera.py
+++ camera.py
@@ -1,22 +1,28 @@
 import cv2, time, os, sys
 from ascii import render, get_html_template
 
-cam = cv2.VideoCapture(0)
-gradient = r' .:!/r(l1Z4H9W8$@'
 
-while True:
-    start_time = time.time()
+def main():
+    cam = cv2.VideoCapture(0)
+    gradient = r' .:!/r(l1Z4H9W8$@'
 
-    y = os.get_terminal_size().lines
-    ret, frame = cam.read()
+    while True:
+        start_time = time.time()
 
-    frame_h, frame_w, _ = frame.shape
-    aspect = frame_w / frame_h
-    symbol_aspect = 7 / 15
-    template = get_html_template()
+        y = os.get_terminal_size().lines
+        ret, frame = cam.read()
 
-    resolution = (round(y * aspect / symbol_aspect), y)
-    frame = cv2.resize(frame, resolution, interpolation=cv2.INTER_AREA)
-    art = render(frame, gradient, f"FPS: {1.0 / (time.time() - start_time)}")
+        frame_h, frame_w, _ = frame.shape
+        aspect = frame_w / frame_h
+        symbol_aspect = 7 / 15
+        template = get_html_template()
 
-    sys.stdout.write(art)
+        resolution = (round(y * aspect / symbol_aspect), y)
+        frame = cv2.resize(frame, resolution, interpolation=cv2.INTER_AREA)
+        art = render(frame, gradient, f"FPS: {1.0 / (time.time() - start_time)}")
+
+        sys.stdout.write(art)
+
+
+if __name__ == '__main__':
+    main()
