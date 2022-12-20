739e3fceda93ab2a99f04b499e8b7e4212e3c9b6
barabum0 <r.m.poltorabatko@gmail.com>
2022-12-20 19:00:10 +0300

    init

---
+++ ascii.py
@@ -0,0 +1,55 @@
+import cv2
+import numpy as np
+
+
+def to_grayscale(r: int, g: int, b: int):
+    # gr = (r + g + b) / 3
+    gr = max(r, g, b)
+    return gr, gr, gr
+
+
+def remap(x, oldMin, oldMax, newMin, newMax):
+    reverseInput = False
+    oldMin = min(oldMin, oldMax)
+    oldMax = max(oldMin, oldMax)
+    if not oldMin == oldMin:
+        reverseInput = True
+
+    reverseOutput = False
+    newMin = min(newMin, newMax)
+    newMax = max(newMin, newMax)
+    if not newMin == newMin:
+        reverseOutput = True
+
+    portion = (x-oldMin)*(newMax-newMin)/(oldMax-oldMin)
+    if reverseInput:
+        portion = (oldMax-x)*(newMax-newMin)/(oldMax-oldMin)
+
+    result = portion + newMin
+    if reverseOutput:
+        result = newMax - portion
+
+    return result
+
+
+def render(img: np.ndarray, gradient: str, debug: str) -> str:
+    art = ""
+    for num_x, x in enumerate(img):
+        for num_y, y in enumerate(x):
+            r, g, b = y
+            gr, gg, gb = to_grayscale(r, g, b)
+            img[num_x, num_y] = [gg, gb, gr]
+
+            to_symbol = round(remap(gr, 0, 255, 0, len(gradient)))
+            art += gradient[to_symbol - 1]
+
+        if num_x == 0:
+            art += str(debug)
+        art += "\n"
+
+    return art
+
+
+def get_html_template() -> str:
+    with open("html_template.html", "r") as f:
+        return f.read()
---
+++ camera.py
@@ -0,0 +1,22 @@
+import cv2, time, os, sys
+from ascii import render, get_html_template
+
+cam = cv2.VideoCapture(0)
+gradient = r' .:!/r(l1Z4H9W8$@'
+
+while True:
+    start_time = time.time()
+
+    y = os.get_terminal_size().lines
+    ret, frame = cam.read()
+
+    frame_h, frame_w, _ = frame.shape
+    aspect = frame_w / frame_h
+    symbol_aspect = 7 / 15
+    template = get_html_template()
+
+    resolution = (round(y * aspect / symbol_aspect), y)
+    frame = cv2.resize(frame, resolution, interpolation=cv2.INTER_AREA)
+    art = render(frame, gradient, f"FPS: {1.0 / (time.time() - start_time)}")
+
+    sys.stdout.write(art)
